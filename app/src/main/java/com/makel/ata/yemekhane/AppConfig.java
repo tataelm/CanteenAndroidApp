5f36ed44d69943f30292ee57bed7b65a06920009
Atahan.Elmacioglu <tataelm@hotmail.com>
2021-03-02 14:17:50 +0300

    Finalize production v.1.1

--- app/src/main/java/com/makel/ata/yemekhane/AppConfig.java
+++ app/src/main/java/com/makel/ata/yemekhane/AppConfig.java
@@ -17,11 +17,11 @@ public class AppConfig {
     public static final List<FoodPlan> _listFoodPlan = new ArrayList<>();
     public static FoodPlan _foodPlan = null;
     public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String BASE_URL = "http://192.168.223.1:8090/foodplan";
+    // public static final String BASE_URL = "http://192.168.223.1:8090/foodplan";
+    // public static final String BASE_URL = "http://10.0.0.101:8188/foodplan";
+    public static final String BASE_URL = "http://85.105.238.124:8188/foodplan";
 
-
-    public static class Settings
-    {
+    public static class Settings {
         private static final String settings_ShowOldPlans = "showOldPlans";
         public static boolean _showOldPlans = false;
 
@@ -30,8 +30,7 @@ public class AppConfig {
             _showOldPlans = sharedPreferences.getBoolean(settings_ShowOldPlans, false);
         }
 
-        public static void saveSettings()
-        {
+        public static void saveSettings() {
             SharedPreferences sharedPreferences = MainActivity.getInstance().getSharedPreferences(AppConfig.SHARED_PREFS, Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putBoolean(settings_ShowOldPlans, _showOldPlans);
